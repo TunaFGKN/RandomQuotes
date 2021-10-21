c3f55ed0b5602266e1753b3f0e78a84c98ffe9d4
TunaFigankaplan <swaxztn@gmail.com>
2021-10-21 22:31:33 +0300

    first commit

---
+++ randomQuotes.py
@@ -0,0 +1,20 @@
+# Librarys needed
+import requests
+from googletrans import Translator
+
+# API
+url = "https://movie-quote-api.herokuapp.com/v1/quote/"
+
+# Getting data
+response = requests.get(url)
+data = response.json()
+
+# Google translator object
+translator = Translator()
+
+# Random quote and show
+quote = data['quote']
+show = data['show']
+
+print("Quote:", quote)
+print("Show:", show)
